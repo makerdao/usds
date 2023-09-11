24e5958c34c0e8f86b9d374b5f70130b100c99a7
sunbreak1211 <129470872+sunbreak1211@users.noreply.github.com>
2023-09-11 13:16:53 -0300

    Change test directory location + Add deployment scripts (#4)
    * Change test directory location + Add deployment scripts
    
    * Rename test
    
    * Remove spacing
    
    * Check vat
    
    * Update deploy/NstDeploy.sol
    
    Co-authored-by: oldchili <130549691+oldchili@users.noreply.github.com>
    
    * Change chainlog keys
    
    ---------
    
    Co-authored-by: oldchili <130549691+oldchili@users.noreply.github.com>

---
+++ deploy/NstInit.sol
@@ -0,0 +1,56 @@
+// SPDX-FileCopyrightText: © 2023 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity >=0.8.0;
+
+import { DssInstance } from "dss-test/MCD.sol";
+import { NstInstance } from "./NstInstance.sol";
+
+interface NstLike {
+    function rely(address) external;
+    function deny(address) external;
+}
+
+interface NstJoinLike {
+    function nst() external view returns (address);
+    function vat() external view returns (address);
+}
+
+interface DaiNstLike {
+    function daiJoin() external view returns (address);
+    function nstJoin() external view returns (address);
+}
+
+library NstInit {
+    function init(
+        DssInstance memory dss,
+        NstInstance memory instance
+    ) internal {
+        require(NstJoinLike(instance.nstJoin).vat() == address(dss.vat), "NstInit/vat-does-not-match");
+        require(NstJoinLike(instance.nstJoin).nst() == instance.nst, "NstInit/nst-does-not-match");
+
+        address daiJoin = dss.chainlog.getAddress("MCD_JOIN_DAI");
+        require(DaiNstLike(instance.daiNst).daiJoin() == daiJoin, "NstInit/daiJoin-does-not-match");
+        require(DaiNstLike(instance.daiNst).nstJoin() == instance.nstJoin, "NstInit/nstJoin-does-not-match");
+
+        NstLike(instance.nst).rely(instance.nstJoin);
+        NstLike(instance.nst).deny(instance.owner);
+
+        dss.chainlog.setAddress("NST",      instance.nst);
+        dss.chainlog.setAddress("NST_JOIN", instance.nstJoin);
+        dss.chainlog.setAddress("DAI_NST",  instance.daiNst);
+    }
+}
